94cc3fd9aa36bc32a4b78666eb71e91828a15c46
AkulaSai93 <saiakula620@gmail.com>
2026-09-25 14:28:53 +0530

    Revert "Add singularity section before FAQ"
    This reverts commit 88fb5bb18ec26c70ced2403952061f5037b4c70a.

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,6 @@ import BuiltToChallenge from '@/components/BuiltToChallenge';
 import HowItWorks from '@/components/HowItWorks';
 import MentorsGrid from '@/components/MentorsGrid';
 import Mentors from '@/components/Mentors';
-import Singularity from '@/components/Singularity';
 import FAQ from '@/components/FAQ';
 import Footer from '@/components/Footer';
 import SectionReveals from '@/components/SectionReveals';
@@ -21,7 +20,6 @@ export default function Home() {
         <HowItWorks />
         <MentorsGrid />
         <Mentors />
-        <Singularity />
         <FAQ />
       </main>
       <Footer />
